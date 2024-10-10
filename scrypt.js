0b5851ae33ce5a7e060e3a8bca0c1a7d053bc365
16010-20-06 <16010-20-06@exchange.ad.afpanet>
2024-10-10 08:28:18 +0200

    1ere version site Pomme

---
+++ scrypt.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Le site de présentation des pommes est chargé.");
+    // Ajoutez ici des fonctionnalités JavaScript si nécessaire
+});
